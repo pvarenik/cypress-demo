5885041ed465270a8eb4b97fb732966ea696dd48
Pavel Varenik <varenikpavel@gmail.com>
2022-10-22 00:22:06 +0200

    ttest

--- cypress/e2e/1-getting-started/todo.cy.js
+++ cypress/e2e/1-getting-started/todo.cy.js
@@ -36,7 +36,7 @@ describe('example to-do app', () => {
 
   it('can add new todo items', () => {
     // We'll store our item text in a variable so we can reuse it
-    const newItem = 'Feed the cat1'
+    const newItem = 'Feed the cat'
 
     // Let's get the input element and use the `type` command to
     // input our new list item. After typing the content of our item,
@@ -54,7 +54,7 @@ describe('example to-do app', () => {
     cy.get('.todo-list li')
       .should('have.length', 3)
       .last()
-      .should('have.text', newItem)
+      .should('have.text', newItem + "1")
   })
 
   it('can check off an item as completed', () => {
