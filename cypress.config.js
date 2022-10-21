21162b37be53b6bd8b896556f26b98c10972fe7f
Pavel Varenik <varenikpavel@gmail.com>
2022-10-21 22:50:00 +0200

    Allure

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,14 @@
-// const allureWriter = require('@shelex/cypress-allure-plugin/writer');
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://example.cypress.io/',
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-      // allureWriter(on, config);
-      // return config;
-    },
-  },
+    e2e: {
+        setupNodeEvents(on, config) {
+            allureWriter(on, config);
+            return config;
+        }
+    }
+  }
 });
